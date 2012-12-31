41248d5524509ed05604c516eebe126f789b7f7d
yangyingchao <yangyingchao@gmail.com>
2012-12-31 18:22:57 +0800

    Updated protocol.

--- src/public/MessageBase.h
+++ src/public/MessageBase.h
@@ -8,8 +8,10 @@ const uint8 TMP_FLAG = 1;
 typedef struct _msg_header
 {
     uint8  type;
-    uint8  reserved[3];
-    uint32 data_size;
+    uint8  attr;
+    uint8  reserved[2];
+    uint8  ahs_legth;
+    uint8  msg_length[3];
 } msg_header;
 
 #define HEADER_LENGTH       sizeof(msg_header)
