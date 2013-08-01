450773cab8ffe931371acbb294d5c1577a41ae7a
yangyingchao <yangyingchao@gmail.com>
2013-08-01 18:45:12 +0800

    auto

--- src/public/python/ZmqWrapper.py
+++ src/public/python/ZmqWrapper.py
@@ -3,6 +3,7 @@
 
 import THMessage_pb2
 import zmq
+import sys
 
 from zmq import REQ, REP, PUB, SUB, PAIR, DEALER, ROUTER, PULL, PUSH, XSUB, XPUB
 
@@ -31,29 +32,84 @@ class ZmqSocket:
     """Wrapper of zmq_socket.
     """
 
-    def __init__(self, type=None, ctx=None):
+    def __init__(self, stype=None, ctx=None):
         """Initializer of ZmqSocket
         """
         self.ctx = ctx
         if ctx is None:
             self.ctx = zmq.Context()
-        self.sock = self.ctx.socket(type)
+        self.sock = self.ctx.socket(stype)
+        self.stype = stype
         pass
 
     def Recv(self):
-        """
+        """Recv a ZmqMessage
         """
         pass
 
     def Send(self, zmsg):
-        """
+        """Send a ZmqMessage
 
         Arguments:
-        - `zmsg`:
+        - `zmsg`: message to be sent.
         """
         pass
 
+    def SendRaw(self, data):
+        """Send raw data
+
+        Arguments:
+        - `data`: raw data to be send
+        """
+        self.sock.send(data)
+
+    def RecvRaw(self):
+        """
+        """
+        return self.sock.recv()
+
+
+    def Bind(self, addr):
+        """Bind to an address
+
+        Arguments:
+        - `addr`: address to be bound.
+        """
+        result = False
+        if self.stype == REP:
+            try:
+                self.sock.bind(addr)
+            except:
+                print sys.exc_info()
+            else:
+                result = True
+        return result
+
+    def Connect(self, addr):
+        """Connect to an address
+
+        Arguments:
+        - `addr`: address to be connected.
+        """
+        result = False
+        if self.stype == REQ:
+            try:
+                self.sock.connect(addr)
+            except:
+                print sys.exc_info()
+            else:
+                result = True
+                return result
 
 if __name__ == '__main__':
-    sock = ZmqSocket(REQ)
-    print("ok\n")
+
+    # 1. Test socket.
+    # 1.1 REP socket.
+    sock = ZmqSocket(REP)
+    ret = sock.Bind("tcp://127.0.0.1:5880")
+    print ret
+
+    # 1.2 REQ socket
+    sock2 = ZmqSocket(REQ)
+    sock2.Connect("tcp://127.0.0.1:5880")
+    print ret
